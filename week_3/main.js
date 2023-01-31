d71940ecf5a1802b90c04f75c4d3d731f7c764cc
Chapagai Santosh <s2300121@BC-C02J63E0Q6LR-MacBook-Air.local>
2023-01-31 12:10:42 +0200

    calculators

--- week_3/main.js
+++ week_3/main.js
@@ -16,18 +16,38 @@ const calcGasoline = () =>{
 };
 
 
-const tempConverter=(id)=>{
+const tempConverter=(id, value)=>{
+
+    const val = parseFloat(value);
+
 const celInput = document.querySelector('#celcius');
 const fahInput = document.querySelector('#fahrenheit');
 const kelInput = document.querySelector('#kelvin');
 
+if(id==='celcius'){
+    fahInput.value = (val * 1.8+32).toFixed(2);
+   kelInput.value = (val + 273.15).toFixed(2);     
+}
 
+if(id==='fahrenheit'){
+celInput.value= ((val-32)/1.8).toFixed(2);
+kelInput.value= ((val-32)/1.8 + 273.15).toFixed(2);
+}
+if(id==='kelvin'){
+    celInput.value= (val-273.15).toFixed(2);
+    fahInput.value= ((val-273.15)*9/5+32).toFixed(2);
+}
+// toFixed helps to fix how many numbers to put after decimals.
 
-
-fahInput.value = (celInput.value)*1.8+32;
-kelInput.value = +(celInput.value)+273.15;
 
 
 
 
 }
+
+
+
+
+
+// const paragraphs = document.querySelectorAll('p');
+// console.log(paragraphs)
