7375cefbf4031331f0c06ebb94681b5f88e131e6
Chapagai Santosh <s2300121@BC-C02J63E0Q6LR-MacBook-Air.local>
2023-02-07 13:02:02 +0200

    Background color generator

--- week_4/main.js
+++ week_4/main.js
@@ -3,10 +3,28 @@ const form = document.querySelector('form');
 const color1 = document.querySelector('#color1');
 const color2 = document.querySelector('#color2');
 
-const backgroundChange= () => {
+const directions = document.querySelectorAll('input[name="direction"]');
+const text = document.querySelector('#cssCode');
 
-    document.body.style.backgroundImage = `linear-gradient(${color1.value}, ${color2.value})`;
-}
+
+const backgroundChange= () => {
+    console.log(directions);
+
+    let dirValue, code;
+
+    for(const item of directions){
+        if(item.checked){
+            dirValue = item.value;
+        }
+    }
+
+    code = `linear-gradient(${dirValue},${color1.value}, ${color2.value})`;
+    document.body.style.backgroundImage = code
+
+    text.textContent = code;
+    
+};
+
 // () this run the function instantly.
 form.addEventListener('change', backgroundChange);
 
