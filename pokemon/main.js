8d8b78a9441ba6d917a964cb5f70bad1e1486ca3
SantoshChapagai <santoshchapagai100@gmail.com>
2023-03-16 22:52:33 +0200

    fetching data

--- pokemon/main.js
+++ pokemon/main.js
@@ -10,20 +10,73 @@
 
 //   }).join('');
 // }
-fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=20')
+// fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=20')
+//   .then(response => response.json())
+//   .then(json => {
+//     const fetches = json.results.map(item => {
+//       return fetch(item.url).then(res => res.json())
+//     })
+//     Promise.all(fetches).then(res => dataList(res))
+//   });
+
+
+// const dataList = (data) => {
+//   document.querySelector('.card').innerHTML = data.map((item) => {
+//     return `<li><img src="${item.sprites.other.dream_world.front_default}"/> <h2>${item.name}</h2></li> `;
+
+//   }).join('');
+// }
+const generations = [
+  { limit: 151, offset: 0 },
+  { limit: 100, offset: 151 },
+  { limit: 135, offset: 251 },
+  { limit: 107, offset: 386 },
+  { limit: 156, offset: 493 },
+  { limit: 72, offset: 649 },
+  { limit: 88, offset: 721 },
+  { limit: 96, offset: 809 },
+  { limit: 110, offset: 905 },
+];
+const button = document.querySelectorAll('.generation');
+button.forEach((button, i) => {
+  button.addEventListener('click', () => fetchData(generations[i], i));
+});
+limit = generations.limit;
+offset = generations.offset;
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
   .then(response => response.json())
-  .then(json => {
-    const fetches = json.results.map(item => {
-      return fetch(item.url).then(res => res.json())
+  .then((data) => {
+    const fetches = data.results.map(data => {
+      return fetch(data.url).then(res => res.json())
     })
     Promise.all(fetches).then(res => dataList(res))
   });
+const clickButton = (i) => {
+  if (i == 0) {
+    console.log(dataList());
+    // return generations[0].dataList();
+  } else if (i == 1) {
+    console.log('what');
+  }
 
+}
 
 const dataList = (data) => {
+
   document.querySelector('.card').innerHTML = data.map((item) => {
-    return `<li><img src="${item.sprites.other.dream_world.front_default}"/> <h2>${item.name}</h2></li> `;
+    return `<li><img src="${item.sprites.other.dream_world.front_default}"/> <h2>${item.name}</h2> <h3>${item.types.map(type => type.type.name)}</h3> <h3>${item.id}</h3> </li> `;
 
   }).join('');
+
 }
+
+
+
+
+
+
+
+
+
+
 
